a19ff2d419c3b9536cb5bff86010685824ca9491
jozefxcabala <xcabala@stuba.sk>
2021-06-01 15:56:23 +0200

    Final commit

--- src/main/java/Cvicenie8.java
+++ src/main/java/Cvicenie8.java
@@ -1,6 +1,6 @@
 public class Cvicenie8 {
     public static void main(String[] args) {
-        //MyWindow1 myWindow1 = new MyWindow1("Uloha 8.1 - 8.3");
+        MyWindow1 myWindow1 = new MyWindow1("Uloha 8.1 - 8.3");
         MyWindow2 myWindow2 = new MyWindow2("Uloha 8.4");
     }
 }
